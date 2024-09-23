f420940848b7c9280828c965233726affa12609d
ngtuantu1202 <125531012+ngtuantu1202@users.noreply.github.com>
2024-09-23 11:30:10 +0700

    Basic

---
+++ Python_Basic/KyTu.py
@@ -0,0 +1,10 @@
+print("\'") #in ra ki tu dac biet
+print(""" """) #in ra man hinh tren nhieu dong
+
+print(" C:\\\ some \t one \"else\" \'2023")
+
+print(""" Usage: thingy[Options]
+            -h:  Display usage message 
+            -H Hostname:  Hostname be connected to 
+    """)
+
